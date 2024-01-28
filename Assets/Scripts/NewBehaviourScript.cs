df7d4b3b14116fdcfa506ec8155a77b626b0b8a8
maximilien <maximilien.jallamion@gmail.com>
2024-01-28 12:51:08 +0100

    folder + trier

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void bstart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
